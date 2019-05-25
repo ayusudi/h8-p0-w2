33fee3b0e4c0f101345c6fa393a3550e99e6f345
ayusudi <ayusudi.abc@gmail.com>
2019-05-25 19:44:24 +0700

    cara I dan II

--- exercise-13.js
+++ exercise-13.js
@@ -1,4 +1,5 @@
-console.log('exercise 13 \n')
+//Cara Pertama//
+console.log('exercise 13, cara I \n')
 
 function xo(str) {
   var jumlahX = str.search ('x');
@@ -8,6 +9,30 @@ function xo(str) {
   else {
     return 'false'
   }
+}
+
+console.log(xo('xoxoxo')); 
+console.log(xo('oxooxo'));
+console.log(xo('oxo'));
+console.log(xo('xxxooo'));
+console.log(xo('xoxooxxo'));
+
+//Cara Kedua//
+console.log ('\nexercise 13, cara II\n')
+
+function xo(strr) {
+var hurufX = 0;
+var hurufO = 0;
+
+for (var i = 0; i <= strr.length; i++){
+  if (strr[i] === 'x') {
+    hurufX+=1;
+  }
+  else if (strr[i] === 'o') {
+    hurufO+=1;
+  }
+}
+return hurufX===hurufO
 }
 
 console.log(xo('xoxoxo')); 
